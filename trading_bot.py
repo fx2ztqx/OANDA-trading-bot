ffa95c36232fc1597d61375c3e52ffd2be453d92
Gleb <shevchukgv@gmail.com>
2015-08-23 23:49:14 +0300

    Fix sys.argv

--- trading_bot.py
+++ trading_bot.py
@@ -16,6 +16,17 @@ import numpy as np
 import time
 import datetime
 
+
+if __name__ == "__main__":
+    if len(sys.argv) < 4:
+        print "Give me a candle size [1H, 15M], instrument [GBP_JPY, EUR_USD], SMA period [80, 200] and EMA period [80,200]!"
+        sys.exit()
+    else:
+        granularity = sys.argv[1]
+        instrument = sys.argv[2]
+        sma_period= int(sys.argv[3])
+        ema_period = int(sys.argv[4])
+
 server ='api-fxpractice.oanda.com'
 #granularity = 'H1'
 #instrument = 'GBP_JPY'
@@ -25,29 +36,11 @@ now_time = datetime.datetime.now()
 cur_hour = now_time.hour # Час текущий
 cur_minute = now_time.minute # Минута текущая
 
-#start_hour = cur_hour - 1
 
-#d = [1,2,3,4,5,7,2,3,4,1,2,6,5,3,4,6]
+d = [1,2,3,4,5,7,2,3,4,1,2,6,5,3,4,6]
 #m = []
 
-is_dst = time.daylight and time.localtime().tm_isdst > 0
-utc_offset = (time.altzone if is_dst else time.timezone)
-def getGranularitySeconds(granularity):
-    if granularity[0] == 'S':
-        return int(granularity[1:])
-    elif granularity[0] == 'M' and len(granularity) > 1:
-        return 60*int(granularity[1:])
-    elif granularity[0] == 'H':
-        return 60*60*int(granularity[1:])
-    elif granularity[0] == 'D':
-        return 60*60*24
-    elif granularity[0] == 'W':
-        return 60*60*24*7
-    #Does not take into account actual month length
-    elif granularity[0] == 'M':
-        return 60*60*24*30
 
-#Надо сделать, чтоб постоянно запрашивал closeMid
 def connection():
  global candle
  headers = {'Authorization' : 'Bearer ' + "c1d60b84697cdc6e0c26684a42e54190-13a1e440394be03e6723bc09b4b75955"}
@@ -58,17 +51,19 @@ def connection():
  #return candle
 connection()
 
+
+#Have to fix
 def strip_candel():
  global closeMid_first, closeMid_second
 
  msg = json.loads(candle)
  data = json.dumps(msg)
- closeMid_first = data[69:77] #191.6275
- closeMid_second = data[226:233] #191.7545 - меняется. надо понять что это
- m.insert(+1, str(closeMid_first))  
+ closeMid_first = data[69:77] 
+ closeMid_second = data[226:233] 
+ #m.insert(+1, str(closeMid_first))  
  return closeMid_first
- return m
- #print data
+ #return m
+
 
 print strip_candel()
 
@@ -87,17 +82,8 @@ def EMA(values, window):
  return a 
 
 #SMA 80 + 200
-print SMA(d, 4)
+print SMA(d, sma_period)
 #EMA 80 + 200
-print EMA(d, 2)
+print EMA(d, ema_period)
 strip_candel()
 
-
-
-if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print "Give me a candle size (1H, 15M) and pair (GBP_JPY, EUR_USD)!"
-        sys.exit()
-    else:
-        granularity, instrument = sys.argv[1:]
-        compareAndTrade(granularity, instrument)
